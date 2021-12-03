599465c1162315101f7715f2e6dca02074be737b
Mark Roxberry <mroxberry@healthverity.com>
2021-12-03 16:36:10 -0500

    Gatsby implementation

---
+++ gatsby-config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "sjs1",
+  },
+  plugins: [
+    // {
+    //   resolve: "gatsby-source-wordpress",
+    //   options: {
+    //     url: "",
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-plugin-google-analytics",
+    //   options: {
+    //     trackingId: "",
+    //   },
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const IndexPage = () => {
+    return (
+      <main>
+        <title>SJS1</title>
+        <h1>SJS1</h1>
+      </main>
+    )
+  }
+  
+  export default IndexPage
+  
