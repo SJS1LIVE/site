0df81932955ba8e9c08b7377986c8ffbf57b894c
Mark Roxberry <mark@un2.tech>
2022-09-27 21:57:04 -0400

    content: change in YouTube

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const IndexPage = () => (
     <Layout>
 
         <h4>Live with Barry Towbin, New Jersey State Referee Administrator</h4>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/ATdD5VNhQbg" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/0mFV_Nr5gec" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <h4>Live with Noah Black, Phil Griffiths, and Thiago Evangelista from the Philadelphia Union</h4>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/A-BQ8s5CS6U" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         <h4>Live with Leigh Povia and Christian Clayton, Center for Dynamic Growth</h4>
